d7475997c5b510871f2a418ada2e0b21bdb9f182
ivoshet <ivoshet.world@gmail.com>
2019-07-30 23:08:02 +0700

    added jsdoc3, express-handlebars

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-// const config = require('./server/configure');
+const config = require('./server/configure');
 const path = require('path');
 
 let app = express();
 
 /**
  * @description setting app
+ * @param {Object} app - is object of express
  */
-const settingApp = () => {
+const settingApp = app => {
   app.set('port', process.env.PORT || 3300);
   app.set('views', path.join(__dirname, '/views'));
+  return app;
   // app = config(app);
 };
 
@@ -17,7 +19,7 @@ const settingApp = () => {
  * @description main function
  */
 const server = () => {
-  settingApp();
+  settingApp(app);
   app.listen(app.get('port'), () => {
     console.log(`server work ${app.get('port')}`);
   });
